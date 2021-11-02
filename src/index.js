a95cca4ce3534df38bf692ba3e530439f4fcd714
Ajayi Peter <peterdgreat2016@gmail.com>
2021-11-02 15:58:34 +0100

    Redux store setup

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
-import { store } from './app/store';
+import { store } from './redux/configureStore';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
