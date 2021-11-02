f0a65c29f7f3e4aeb1ec8d3c1192dc563023f918
Ajayi Peter <peterdgreat2016@gmail.com>
2021-11-02 16:21:21 +0100

    NavBar Component Created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <NavBar />
     </div>
   );
 }
