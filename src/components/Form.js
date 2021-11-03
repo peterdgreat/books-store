43fc23103f3e3473cd434ec6c06a42abc28e41c9
Ajayi Peter <peterdgreat2016@gmail.com>
2021-11-03 22:07:36 +0100

    Add functionality refactored

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,34 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 import { addBook } from '../redux/books/books';
-import useInputHook from '../hooks/inputdata';
 
-require('react-dom');
-window.React2 = require('react');
+// require('react-dom');
+// window.React2 = require('react');
 
-console.log(window.React1 === window.React2);
+// console.log(window.React1 === window.React2);
 
 const Form = () => {
   const dispatch = useDispatch();
-  const { value: titleValue, onChange: titleOnChange } = useInputHook('');
-  const { value: authorValue, onChange: authorOnChange } = useInputHook('');
+  const [book, setBook] = useState();
 
-  const submitBookToStore = (e) => {
-    e.preventDefault();
+  const submitBookToStore = () => {
     const newBook = {
       id: uuidv4(),
-      title: titleValue,
-      author: authorValue,
+      title: book.title,
+      author: book.author,
     };
     dispatch(addBook(newBook));
-    console.log(newBook);
+  };
+  const handleChange = (e) => {
+    setBook({
+      ...book, [e.target.name]: e.target.value,
+    });
   };
   return (
     <form>
-      <input type="text" name="title" onChange={titleOnChange} id="title" placeholder="Add your title" />
+      <input type="text" name="title" onChange={handleChange} id="title" placeholder="Add your title" />
       <br />
-      <input type="text" name="Author" onChange={authorOnChange} id="author" placeholder="Authors name" />
+      <input type="text" name="author" onChange={handleChange} id="author" placeholder="Authors name" />
       <button type="button" onClick={submitBookToStore}>Add Book</button>
     </form>
   );
