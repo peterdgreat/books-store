25c6cbe87e7f98696d021212470fa1c3a3c55ab1
Ajayi Peter <peterdgreat2016@gmail.com>
2021-11-04 14:52:43 +0100

    API Call error

--- src/components/BooksContainer.js
+++ src/components/BooksContainer.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import BookLists from './BookLists';
 import Form from './Form';
+import { getBooks } from '../redux/books/books';
 
 const BooksContainer = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getBooks());
+  }, []);
   const Books = useSelector((state) => state.booksReducer.books);
   return (
     <div className="container">
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,33 +1,68 @@
 /* eslint-disable no-undef */
-const ADD_BOOK = 'bookStore/books/ADD_BOOk';
-const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+const GET_BOOK_SUCCESS = 'bookStore/books/BOOK_SUCCESS';
+
+// const ADD_BOOK = 'bookStore/books/ADD_BOOk';
+// const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+const appId = 'MFla1FZXtNvW7w7zQJyI';
+const url = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${appId}/books`;
 
 const initialState = {
   books: [],
 };
 
-export const addBook = (payload) => ({
-  type: ADD_BOOK,
-  payload,
-});
+export const getBooks = () => (dispatch) => {
+  fetch(url).then((response) => {
+    response.json().then((data) => {
+      dispatch({
+        type: GET_BOOK_SUCCESS,
+        payload: data,
+      });
+    });
+  });
+};
 
-export const removeBook = (payload) => ({
-  type: REMOVE_BOOK,
-  payload,
-});
+export const addBook = (payload) => (dispatch) => {
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      contentType: 'application/json',
+    },
+    body: JSON.stringify(payload),
+
+  }).then((response) => {
+    if (response.status !== 200) {
+      console.log(`Looks like there was a problem. Status Code: ${response.status}`);
+      return;
+    }
+    response.json().then((data) => {
+      dispatch({
+        type: GET_BOOK_SUCCESS,
+        payload: data,
+      });
+    });
+  });
+};
+
+export const removeBook = (payload) => (dispatch) => {
+  fetch(`${url}/${payload}`, {
+    method: 'DELETE',
+    headers: {
+      contentType: 'application/json',
+    },
+  }).then(() => {
+    dispatch({
+      type: GET_BOOK_SUCCESS,
+      payload,
+    });
+  });
+};
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case ADD_BOOK:
+    case GET_BOOK_SUCCESS:
       return {
         ...state,
         books: [...state.books, action.payload],
-      };
-
-    case REMOVE_BOOK:
-      return {
-        ...state,
-        books: state.books.filter((book) => book.id !== action.payload),
       };
     default:
       return state;
