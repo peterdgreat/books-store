724aeeba67b415a4f872ab33ec641ad9169bf59d
Ajayi Peter <peterdgreat2016@gmail.com>
2021-11-04 18:46:14 +0100

    Delete and Add functionality from API

--- src/components/BooksContainer.js
+++ src/components/BooksContainer.js
@@ -1,32 +1,25 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import BookLists from './BookLists';
 import Form from './Form';
-import { getBooks } from '../redux/books/books';
 
 const BooksContainer = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(getBooks());
-  }, []);
   const Books = useSelector((state) => state.booksReducer.books);
   return (
-    <div className="container">
+    <div>
 
-      <section className="row">
-        <ul className="col-md-6 ">
-          {Books.map((book) => (
-            <BookLists
-              key={book.id}
-              id={book.id}
-              author={book.author}
-              title={book.title}
-              category={book.category}
-            />
-          ))}
+      <ul>
+        {Books.map((book) => (
+          <BookLists
+            key={book.id}
+            id={book.id}
+            author={book.author}
+            title={book.title}
+            category={book.category}
+          />
+        ))}
 
-        </ul>
-      </section>
+      </ul>
       <Form />
     </div>
   );
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,68 +1,61 @@
 /* eslint-disable no-undef */
-const GET_BOOK_SUCCESS = 'bookStore/books/BOOK_SUCCESS';
+const ADD_BOOK = 'bookStore/books/ADD_BOOk';
+const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
 
-// const ADD_BOOK = 'bookStore/books/ADD_BOOk';
-// const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
-const appId = 'MFla1FZXtNvW7w7zQJyI';
-const url = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${appId}/books`;
+const APP_ID = 'G6Nd2Pu54IbbPFQeXivx';
+const APP_URL = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${APP_ID}/books`;
 
 const initialState = {
   books: [],
 };
 
-export const getBooks = () => (dispatch) => {
-  fetch(url).then((response) => {
-    response.json().then((data) => {
-      dispatch({
-        type: GET_BOOK_SUCCESS,
-        payload: data,
-      });
-    });
-  });
-};
-
-export const addBook = (payload) => (dispatch) => {
-  fetch(url, {
+const postBook = async (payload) => {
+  const book = {
+    item_id: payload.id,
+    title: payload.title,
+    category: payload.category,
+  };
+  const response = await fetch(APP_URL, {
     method: 'POST',
     headers: {
-      contentType: 'application/json',
+      'Content-Type': 'application/json; charset=UTF-8',
     },
-    body: JSON.stringify(payload),
-
-  }).then((response) => {
-    if (response.status !== 200) {
-      console.log(`Looks like there was a problem. Status Code: ${response.status}`);
-      return;
-    }
-    response.json().then((data) => {
-      dispatch({
-        type: GET_BOOK_SUCCESS,
-        payload: data,
-      });
-    });
+    body: JSON.stringify(book),
   });
+  return response.text().then((text) => console.log(text));
 };
 
-export const removeBook = (payload) => (dispatch) => {
-  fetch(`${url}/${payload}`, {
+const deleteBook = async (payload) => {
+  const response = await fetch(`${APP_URL}/${payload}`, {
     method: 'DELETE',
-    headers: {
-      contentType: 'application/json',
-    },
-  }).then(() => {
-    dispatch({
-      type: GET_BOOK_SUCCESS,
-      payload,
-    });
   });
+  return response.text().then((text) => console.log(text));
 };
+
+export const addBook = (payload) => ({
+  type: ADD_BOOK,
+  payload,
+});
+
+export const removeBook = (payload) => ({
+  type: REMOVE_BOOK,
+  payload,
+});
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case GET_BOOK_SUCCESS:
+    case ADD_BOOK:
+      postBook(action.payload);
       return {
         ...state,
         books: [...state.books, action.payload],
+      };
+
+    case REMOVE_BOOK:
+      deleteBook(action.payload);
+      return {
+        ...state,
+        books: state.books.filter((book) => book.id !== action.payload),
       };
     default:
       return state;
