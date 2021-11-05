9a925a76e052ca380cc0b6d6c1a15bca1d4e72e3
Ajayi Peter <peterdgreat2016@gmail.com>
2021-11-05 22:30:15 +0100

    BookList Component styled

--- src/components/BooksContainer.js
+++ src/components/BooksContainer.js
@@ -1,5 +1,7 @@
+import './styles/BookContainer.css';
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+
 import BookLists from './BookLists';
 import Form from './Form';
 import { getBooks } from '../redux/books/books';
@@ -24,12 +26,15 @@ const BooksContainer = () => {
     )));
   });
   return (
-    <div>
+    <div className="container pt-5">
 
-      <ul>
-        {BookList}
+      <div>
+        <ul>
+          {BookList}
 
-      </ul>
+        </ul>
+
+      </div>
       <Form />
     </div>
   );
